f71062c9a1038c60acaa1edf4c5b263a80b5a143
Reynold Figueroa <reynold0801@outlook.com>
2019-05-09 21:03:57 -0400

    .

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ import mustache from 'mustache'
 async function printPDF(template, data, optionsPDF) {
     const browser = await puppeteer.launch({headless: true})
     const page = await browser.newPage()
-    // let html_str = fs.readFileSync(template)
+    let html_str = fs.readFileSync(template)
     // console.log(html_str)
-    // let html_rendered = ejs.render(html_str.toString(), data)
-    let html_rendered = mustache.render(template, data)
+    let html_rendered = ejs.render(html_str.toString(), data)
+    // let html_rendered = mustache.render(template, data)
     console.log(html_rendered)
     await page.goto(`data:text/html, ${html_rendered}`, {
         waitUntil: ['load', 'domcontentloaded', 'networkidle0']
--- test.js
+++ test.js
@@ -2,24 +2,171 @@ import printPDF from './index'
 import path from 'path';
 import fs from 'fs'
 
-const template = './templates/example.html'
+// const template = './templates/conduce.ejs'
+const template = './templates/invoiceCredito.ejs'
 const options = {
-    preferCSSPageSize: true,
-    path: path.resolve('pdf', 'example.pdf')
+    format: 'A4',
+    margin: {
+        top: '24px',
+        bottom: '24px',
+        left: '14px',
+        right: '14px',
+    },
+    path: path.resolve('pdf', 'example2.pdf')
 }
 const data = {
-    greetings: 'Hola Mundo',
-    date: '20/06/1993',
-    items: [
-        {itemNumber: 1},
-        {itemNumber: 2},
-        {itemNumber: 3},
-        {itemNumber: 4},
-        {itemNumber: 5},
-        {itemNumber: 6},
-        {itemNumber: 7}
+    // greetings: 'Hola Mundo',
+    // date: '20/06/1993',
+    // items: [
+    //     {itemNumber: 1},
+    //     {itemNumber: 2},
+    //     {itemNumber: 3},
+    //     {itemNumber: 4},
+    //     {itemNumber: 5},
+    //     {itemNumber: 6},
+    //     {itemNumber: 7}
+    // ],
+
+    id: '32714',
+
+    conduce_id: '32714',
+
+    fecha_formateada: '5/9/2019',
+
+    direccion: {
+        nombre: 'Direccion'
+    },
+
+    empleado: {
+        nombre: 'Nombre',
+        apellidos: 'Apellido Apellido'
+    },
+
+    cliente: {
+        id: 'CLT3468',
+        nombre: 'Nombre',
+        apellidos: 'Apellido Apellido',
+        direccion: {
+            nombre: 'Direccion'
+        },
+        telefono: '(787) 215 5552',
+        email: 'cliente@mail.com'
+    },
+
+    metodo_de_pago: 'crédito',
+
+    carrito: [
+        {
+            id: 1,
+            marca: 'Prueba',
+            modelo: 'Probando',
+            descripcion: 'Probando 123',
+            precio_vendido: '$999.99'
+        },
+        {
+            id: 2,
+            marca: 'Prueba',
+            modelo: 'Probando',
+            descripcion: 'Probando 123',
+            precio_vendido: '$999.99'
+        },
+        {
+            id: 3,
+            marca: 'Prueba',
+            modelo: 'Probando',
+            descripcion: 'Probando 123',
+            precio_vendido: '$999.99'
+        },
+
+        {
+            id: 1,
+            marca: 'Prueba',
+            modelo: 'Probando',
+            descripcion: 'Probando 123',
+            precio_vendido: '$999.99'
+        },
+        {
+            id: 2,
+            marca: 'Prueba',
+            modelo: 'Probando',
+            descripcion: 'Probando 123',
+            precio_vendido: '$999.99'
+        },
+        {
+            id: 3,
+            marca: 'Prueba',
+            modelo: 'Probando',
+            descripcion: 'Probando 123',
+            precio_vendido: '$999.99'
+        },
+        {
+            id: 1,
+            marca: 'Prueba',
+            modelo: 'Probando',
+            descripcion: 'Probando 123',
+            precio_vendido: '$999.99'
+        },
+        {
+            id: 2,
+            marca: 'Prueba',
+            modelo: 'Probando',
+            descripcion: 'Probando 123',
+            precio_vendido: '$999.99'
+        },
+        {
+            id: 3,
+            marca: 'Prueba',
+            modelo: 'Probando',
+            descripcion: 'Probando 123',
+            precio_vendido: '$999.99'
+        },
+        {
+            id: 1,
+            marca: 'Prueba',
+            modelo: 'Probando',
+            descripcion: 'Probando 123',
+            precio_vendido: '$999.99'
+        },
+        {
+            id: 2,
+            marca: 'Prueba',
+            modelo: 'Probando',
+            descripcion: 'Probando 123',
+            precio_vendido: '$999.99'
+        },
+        {
+            id: 3,
+            marca: 'Prueba',
+            modelo: 'Probando',
+            descripcion: 'Probando 123',
+            precio_vendido: '$999.99'
+        },
+        {
+            id: 1,
+            marca: 'Prueba',
+            modelo: 'Probando',
+            descripcion: 'Probando 123',
+            precio_vendido: '$999.99'
+        },
+        {
+            id: 2,
+            marca: 'Prueba',
+            modelo: 'Probando',
+            descripcion: 'Probando 123',
+            precio_vendido: '$999.99'
+        },
+        {
+            id: 3,
+            marca: 'Prueba',
+            modelo: 'Probando',
+            descripcion: 'Probando 123',
+            precio_vendido: '$999.99'
+        },
     ],
-    style: fs.readFileSync('./templates/example.css').toString()
+
+    total: '$999.99',
+
+    mensaje: '¿Dime socio quien tu eres?'
 }
 
 printPDF(template, data, options)
